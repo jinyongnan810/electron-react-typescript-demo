14ee7c9fc7279d1dea8f5d317aca89f99739b0a5
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-19 16:28:28 +0900

    initial frontend

---
+++ README.md
@@ -0,0 +1,3 @@
+### Credits
+
+I have followed this [awesome tutorial](https://dev.to/franamorim/tutorial-reminder-widget-with-electron-react-1hj9)
---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sass/index.scss";
+
+const app = () => {
+  return <div className="container">React Content</div>;
+};
+
+export default app;
