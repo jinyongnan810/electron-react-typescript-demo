01e108abda941bdbad5b70736142f0a4775e99ac
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-24 16:00:32 +0900

    show dev-tools & proper start/ending

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,18 +1,65 @@
-import { app, BrowserWindow } from "electron";
-import isDev from "electron-is-dev"; // New Import
-
-const createWindow = (): void => {
-  let win = new BrowserWindow({
+import { app, BrowserWindow, Menu } from "electron";
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS,
+} from "electron-devtools-installer";
+import isDev from "electron-is-dev";
+const isMac = process.platform === "darwin" ? true : false;
+let mainWindow: BrowserWindow | null;
+const createMainWindow = (): void => {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
   });
-  // win.loadURL(
-  //   isDev ? "http://localhost:9000" : `file://${app.getAppPath()}/index.html`
-  // );
-  win.loadURL(`file://${app.getAppPath()}/index.html`);
-};
+  mainWindow.loadURL(
+    isDev ? "http://localhost:9000" : `file://${app.getAppPath()}/index.html`
+  );
+  // win.loadURL(`file://${app.getAppPath()}/index.html`);
+  if (isDev)
+    installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS]).catch((err) =>
+      console.log("Error loading React DevTools: ", err)
+    ); //install devtools
+  // menu
+  const mainMenu = Menu.buildFromTemplate(menu as any);
+  Menu.setApplicationMenu(mainMenu);
 
-app.on("ready", createWindow);
+  mainWindow.on("closed", () => (mainWindow = null));
+};
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    role: "fileMenu",
+  },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("ready", createMainWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
+
+// Stop error
+app.allowRendererProcessReuse = true;
