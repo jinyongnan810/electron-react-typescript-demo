7c3822a0313566310d0df84485fea26336a50ef9
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-25 08:47:37 +0900

    backend authentications

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+require("express-async-errors");
+import { json } from "body-parser";
+
+import cookieSesion from "cookie-session";
+
+import CurrentUserRouter from "./routers/current-user";
+import SignInRouter from "./routers/signin";
+import SignOutRouter from "./routers/signout";
+import SignUpRouter from "./routers/signup";
+import { NotFoundError } from "./errors/not-found-error";
+import { handleError } from "./middlewares/error-handler";
+
+const app = express();
+app.set("trust proxy", true); //trust ingress nginx
+app.use(json());
+app.use(
+  cookieSesion({
+    signed: false, // no encryption
+    secure: process.env.NODE_ENV === "production", // only https
+  })
+);
+
+app.use(CurrentUserRouter);
+app.use(SignInRouter);
+app.use(SignOutRouter);
+app.use(SignUpRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(handleError);
+
+export { app };
---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import { app } from "./app";
+
+const start = async () => {
+  console.log("Backend starting...");
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY not set.");
+  }
+  try {
+    await mongoose.connect(
+      `mongodb+srv://jinyongnan:${process.env.MONGO_PWD}@cluster0.xk5om.gcp.mongodb.net/electron-full-demo?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    console.log("DB connected.");
+  } catch (error) {
+    console.log(error.messsage);
+  }
+
+  app.listen(4000, async () => {
+    console.log("Backend listening on port 4000.");
+  });
+};
+
+start();
