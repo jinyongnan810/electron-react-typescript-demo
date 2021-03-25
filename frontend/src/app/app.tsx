811d0275b9b80175393993c483272e2a2a9c0b5a
Yuunan Kin <yuunan.kin@gmail.com>
2021-03-25 10:22:29 +0900

    import bootstrap & some markup

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Router } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import "./sass/index.scss";
 
 const app = () => {
-  return <div className="container">React Content</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <div className="container-fluid"></div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default app;
---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Electron-React
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/dashboard" className="nav-link">
+                TO_BE_FILLED's Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Sign Out
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link">
+                Sign Up
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/signin" className="nav-link">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
