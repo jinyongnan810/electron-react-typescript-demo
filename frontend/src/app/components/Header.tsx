3c5a22b54f56fb258d5c2c0209158f07cd8a9a3e
jinyongnan810 <919142075@qq.com>
2021-04-01 17:26:13 +0900

    change title

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          Electron-React
+          Simple-Chat
         </Link>
         <button
           className="navbar-toggler"
