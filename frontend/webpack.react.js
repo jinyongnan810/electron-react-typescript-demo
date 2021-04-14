c9649255c2018dfdffd8d584b0a7105dfdcca072
jinyongnan810 <919142075@qq.com>
2021-04-14 10:43:14 +0900

    update electron webpack

--- frontend/webpack.react.js
+++ frontend/webpack.react.js
@@ -1,48 +1,59 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
 
 const rootPath = path.resolve(__dirname, '.')
 
-module.exports = {
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    mainFields: ['main', 'module', 'browser']
-  },
-  entry: path.resolve(rootPath, 'src', 'index.tsx'),
-  target: 'electron-renderer',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+module.exports = () => {
+  // replace process.env.*
+  const env = dotenv.config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+  return {
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      mainFields: ['main', 'module', 'browser']
+    },
+    entry: path.resolve(rootPath, 'src', 'index.tsx'),
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ]
+    },
+    devServer: {
+      contentBase: path.join(rootPath, 'dist/renderer'),
+      historyApiFallback: true,
+      compress: true,
+      hot: true,
+      host: '0.0.0.0',
+      port: 4000,
+      publicPath: '/'
+    },
+    output: {
+      path: path.resolve(rootPath, 'dist/renderer'),
+      filename: 'js/[name].js',
+      publicPath: './'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      new webpack.DefinePlugin(envKeys)
     ]
-  },
-  devServer: {
-    contentBase: path.join(rootPath, 'dist/renderer'),
-    historyApiFallback: true,
-    compress: true,
-    hot: true,
-    host: '0.0.0.0',
-    port: 4000,
-    publicPath: '/'
-  },
-  output: {
-    path: path.resolve(rootPath, 'dist/renderer'),
-    filename: 'js/[name].js',
-    publicPath: './'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    })
-  ]
+  }
 }
